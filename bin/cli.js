e453140a8de50235e7de51bb80d9864b9967c2df
Darcy Clarke <darcy@darcyclarke.me>
2022-03-09 18:29:32 -0500

    fix: cli string parsing

--- bin/cli.js
+++ bin/cli.js
@@ -24,7 +24,6 @@ function run (argv = process.argv) {
 // usage
 if (isTTY && mainArgs().length === 0) {
   console.error('usage error: no args')
-
   process.exit(1)
 
 // args
@@ -33,11 +32,35 @@ if (isTTY && mainArgs().length === 0) {
 // stdin
 } else {
   let data = ''
+  let argv = []
   stdin.on('data', (chunk) => {
     data += chunk
   })
   stdin.on('end', () => {
-    run(data.toString())
+    // Regular Expression & string parsing from: https://github.com/mccormicka/string-argv
+    // ([^\s'"]([^\s'"]*(['"])([^\3]*?)\3)+[^\s'"]*) Matches nested quotes until the first space outside of quotes
+    // [^\s'"]+ or Match if not a space ' or "
+    // (['"])([^\5]*?)\5 or Match "quoted text" without quotes
+    // `\3` and `\5` are a backreference to the quote style (' or ") captured
+    const regEx = /([^\s'"]([^\s'"]*(['"])([^\3]*?)\3)+[^\s'"]*)|[^\s'"]+|(['"])([^\5]*?)\5/gi
+    function firstString(...args) {
+      for (let i = 0; i < args.length; i++) {
+        const arg = args[i]
+        if (typeof arg === 'string') {
+          return arg
+        }
+      }
+    }
+    do {
+      // Each call to exec returns the next regex match as an array
+      match = regEx.exec(data.toString())
+      if (match !== null) {
+        // Index 1 in the array is the captured group if it exists
+        // Index 0 is the matched text, which we use if no captured group exists
+        argv.push(firstString(match[1], match[6], match[0]))
+      }
+    } while (match !== null)
+    run(argv)
     process.exit(0)
   })
 }
